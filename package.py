d2ca865308a7d0252cc6017abacaa83c8aefacab
han0 <wslshanlin@sina.com>
2020-11-11 09:09:44 +0800

    init: initial commit

---
+++ package.py
@@ -0,0 +1,10 @@
+
+package = {
+    'name': 'nc_http',
+    'version': '0.0.1',
+    'author': 'han0',
+    'email': 'wslshanlin@sina.com',
+    'scripts': {
+        'default': 'echo 请输入明确的命令名称'
+    }
+}
